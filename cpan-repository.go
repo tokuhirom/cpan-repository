55c3dfafa0be457dd5f7be4b02901dce69fe8787
mattn <mattn.jp@gmail.com>
2014-06-23 17:10:44 +0900

    Use go-scan. Check core module

--- cpan-repository.go
+++ cpan-repository.go
@@ -2,54 +2,15 @@ package main
 
 import (
 	"bufio"
-	"encoding/json"
-	"errors"
 	"fmt"
 	"io"
-	"io/ioutil"
 	"log"
 	"net/http"
 	"net/url"
 	"os"
-	"reflect"
-	"regexp"
-	"strconv"
-	"strings"
+
+	"github.com/mattn/go-scan"
 )
-
-var re = regexp.MustCompile("^([^0-9\\s\\[][^\\s\\[]*)?(\\[[0-9]+\\])?$")
-
-func jsonpath(b []byte, jp string, t interface{}) error {
-	var v interface{}
-	err := json.Unmarshal([]byte(b), &v)
-	if err != nil {
-		return err
-	}
-	if jp == "" {
-		return errors.New("invalid path")
-	}
-	for _, token := range strings.Split(jp, "/") {
-		sl := re.FindAllStringSubmatch(token, -1)
-		if len(sl) == 0 {
-			return errors.New("invalid path")
-		}
-		ss := sl[0]
-		if ss[1] != "" {
-			v = v.(map[string]interface{})[ss[1]]
-		}
-		if ss[2] != "" {
-			i, err := strconv.Atoi(ss[2][1 : len(ss[2])-1])
-			if err != nil {
-				return errors.New("invalid path")
-			}
-			v = v.([]interface{})[i]
-		}
-	}
-	rt := reflect.ValueOf(t).Elem()
-	rv := reflect.ValueOf(v)
-	rt.Set(rv)
-	return nil
-}
 
 func getRepositoryUrl(module string) (repositoryUrl string, err error) {
 	var s string
@@ -61,8 +22,11 @@ func getRepositoryUrl(module string) (repositoryUrl string, err error) {
 	}
 	defer resp.Body.Close()
 	if resp.StatusCode == 200 {
-		body, err := ioutil.ReadAll(resp.Body)
-		err = jsonpath(body, "/release/_source/resources/repository/url", &s)
+		err = scan.ScanJSON(resp.Body, "/distribution", &s)
+		if s == "perl" {
+			return "", fmt.Errorf("%q is provided by core module", module)
+		}
+		err = scan.ScanJSON(resp.Body, "/release/_source/resources/repository/url", &s)
 		if err != nil {
 			return "", err
 		}
